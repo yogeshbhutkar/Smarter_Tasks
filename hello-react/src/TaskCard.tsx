6b146b394534c81d8c830cb10d296789e18fee29
Yogesh Bhutkar <pseudonetwork1001@gmail.com>
2023-04-23 20:08:15 +0530

    fix 9

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -12,7 +12,7 @@ const TaskCard = (props: TaskCardInterface) => {
   return (
     <div className="TaskItem">
       <h2 className="text-xl font-bold">{props.title}</h2>
-      {props.dueDate && <p>Due On: {props.dueDate}</p>}
+      {props.dueDate && <p>Due on: {props.dueDate}</p>}
       {props.completedAtDate && <p>Completed on: {props.completedAtDate}</p>}
       <p>Assignee: {props.assigneeName}</p>
     </div>
