19a09f1394354fa6923302d4ccef42cbad79f61c
Yogesh Bhutkar <pseudonetwork1001@gmail.com>
2023-04-23 19:30:51 +0530

    Fix 1

--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -17,13 +17,13 @@ function App() {
             <h1 className="font-bold pb-2 ">Pending</h1>
             <TaskCard
               title="Build the website with static content"
-              dueDate={`10th April`}
-              assigneeName={`Rohit S`}
+              dueDate="10th April"
+              assigneeName="Rohit S"
             />
             <TaskCard
               title="Add blog"
-              dueDate={`22nd March`}
-              assigneeName={`Rohit M`}
+              dueDate="22nd March"
+              assigneeName="Rohit M"
             />
             <button className="bg-purple-400 hover:bg-purple-500 w-full py-2 rounded-lg mb-2 text-white">
               New Task
@@ -33,13 +33,13 @@ function App() {
             <h1 className="font-bold pb-2">Done</h1>
             <TaskCard
               title="Design the mockup"
-              completedAtDate={`10th April`}
-              assigneeName={`Rohit M`}
+              completedAtDate="10th April"
+              assigneeName="Rohit M"
             />
             <TaskCard
               title="Get approval from principal"
-              assigneeName={`Ajay S`}
-              completedAtDate={`20th April`}
+              assigneeName="Ajay S"
+              completedAtDate="20th April"
             />
           </div>
         </div>
